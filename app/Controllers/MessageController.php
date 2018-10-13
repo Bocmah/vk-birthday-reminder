54d7aaa823ce6bfc5fa97a2893912799bf331c4c
Artem <atrushkin1@gmail.com>
2018-10-13 22:18:42 +0300

    Returning responses

--- app/Controllers/MessageController.php
+++ app/Controllers/MessageController.php
@@ -3,6 +3,7 @@
 namespace VkBirthdayReminder\Controllers;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class MessageController
 {
@@ -11,16 +12,16 @@ class MessageController
         $data = json_decode($request->getContent());
 
         if (!$data) {
-            return "nioh";
+            return new Response("nioh");
         }
 
         if ($data->secret !== getenv("VK_SECRET_KEY") && $data->type !== "confirmation") {
-            return "nioh";
+            return new Response("nioh");
         }
 
         switch ($data->type) {
             case "confirmation":
-                return getenv("VK_CONFIRMATION_KEY");
+                return new Response(getenv("VK_CONFIRMATION_KEY"));
                 break;
             case "message_new":
                 break;
