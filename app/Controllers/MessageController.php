6d00113d9443c4f68e23fbe92ecfc88d453c5bdf
Artem <atrushkin1@gmail.com>
2018-10-13 22:13:13 +0300

    Testing VK confirmation

--- app/Controllers/MessageController.php
+++ app/Controllers/MessageController.php
@@ -9,5 +9,21 @@ class MessageController
     public function store(Request $request)
     {
         $data = json_decode($request->getContent());
+
+        if (!$data) {
+            return "nioh";
+        }
+
+        if ($data->secret !== getenv("VK_SECRET_KEY") && $data->type !== "confirmation") {
+            return "nioh";
+        }
+
+        switch ($data->type) {
+            case "confirmation":
+                return getenv("VK_CONFIRMATION_KEY");
+                break;
+            case "message_new":
+                break;
+        }
     }
 }
