69b7b7b291687718516b1dd53d875efab41718c5
Artem <atrushkin1@gmail.com>
2018-11-10 20:57:53 +0300

    Included helpers

---
+++ app/Helpers/MessageSender.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace VkBirthdayReminder\Helpers;
+
+class MessageSender
+{
+    /**
+     * Send a message to a user of $userId
+     *
+     * @param string $txt
+     * @param $userId
+     */
+    public function send(string $txt, $userId)
+    {
+        $requestParams = [
+            "user_id" => $userId,
+            "message" => $txt,
+            "access_token" => getenv("VK_TOKEN"),
+            "v" => getenv("VK_API_VERSION")
+        ];
+        file_get_contents(
+            "https://api.vk.com/method/messages.send?" . http_build_query($requestParams)
+        );
+    }
+}
---
+++ app/Helpers/UsersRetriever.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace VkBirthdayReminder\Helpers;
+
+class UsersRetriever
+{
+    /**
+     * Retrieve VK user by $id
+     * Returns JSON decoded object or associative array if $returnAssoc is true
+     *
+     * @param $id
+     * @param bool $returnAssoc
+     * @return bool|string
+     */
+    public function getUser($id, bool $returnAssoc = false)
+    {
+        $requestParams = [
+          "user_ids" => $id,
+          "v" => getenv("VK_API_VERSION")
+        ];
+
+        $result = file_get_contents(
+            "https://api.vk.com/method/users.get?" . http_build_query($requestParams)
+        );
+
+        return json_decode($result, $returnAssoc);
+    }
+}
