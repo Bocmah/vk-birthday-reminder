95eb302a4f4e157469aab8b3819a417bb278841e
Artem <atrushkin1@gmail.com>
2018-12-09 16:21:38 +0300

    (feat) implement test version of notify cron script

---
+++ cron/notify.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__.'/../bootstrap.php';
+
+$entityManager = $container->getParameter('entity_manager');
+$messageSender = $container->get('msg_sender');
+
+$observers = $entityManager->getRepository('VkBirthdayReminder\Entities\Observer')->findAll();
+
+foreach ($observers as $observer) {
+    $message = "Список юзеров, за которыми вы следите:\n\n";
+    $observees = $observer->getObservees();
+
+    $observees->map(function ($observee) use (&$message) {
+       $observeeFullName = $observee->getFirstName() . $observee->getLastName();
+       $message .= $observeeFullName . "\n";
+    });
+
+    $messageSender->send($message, $observer->getVkId());
+}
