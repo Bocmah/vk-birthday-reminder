be63fe3999c2e9b1aa4912f4754b47724ac5da80
Artem <atrushkin1@gmail.com>
2019-01-12 23:00:49 +0300

    (feat) PHPUnit initial setup

---
+++ tests/ObserveeDataRetrieverTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace VkBirthdayReminder\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ObserveeDataRetrieverTest extends TestCase
+{
+    protected $observeeDataRetriever;
+
+    public function testTrueIsTrue()
+    {
+        $foo = true;
+
+        $this->assertTrue($foo);
+    }
+}
