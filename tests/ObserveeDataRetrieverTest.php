d201db32352803274750772dd191a5454ee9905c
Artem <atrushkin1@gmail.com>
2019-01-13 20:49:27 +0300

    (test) ObserveeDataRetriever::getBirthdayFromMessage

--- tests/ObserveeDataRetrieverTest.php
+++ tests/ObserveeDataRetrieverTest.php
@@ -3,15 +3,50 @@
 namespace VkBirthdayReminder\Tests;
 
 use PHPUnit\Framework\TestCase;
+use VkBirthdayReminder\Helpers\ObserveeDataRetriever;
+use VkBirthdayReminder\Helpers\UserRetriever;
 
 class ObserveeDataRetrieverTest extends TestCase
 {
-    protected $observeeDataRetriever;
-
-    public function testTrueIsTrue()
+    /**
+     * @test
+     */
+    public function getBirthdayFromMessageHandlesMessageWhichDoesNotContainRequiredNumberOfWordsCorrectly()
     {
-        $foo = true;
+        $userRetrieverStub = $this->createMock(UserRetriever::class);
+        $observeeDataRetriever = new ObserveeDataRetriever($userRetrieverStub);
+        $message = 'add 1';
 
-        $this->assertTrue($foo);
+        $this->expectException(\LengthException::class);
+
+        $observeeDataRetriever->getBirthdayFromMessage($message);
+    }
+
+    /**
+     * @test
+     */
+    public function getBirthdayFromMessagesHandlesIncorrectBirthdayPartCorrectly()
+    {
+        $userRetrieverStub = $this->createMock(UserRetriever::class);
+        $observeeDataRetriever = new ObserveeDataRetriever($userRetrieverStub);
+        $message = 'add 1 13.10';
+
+        $this->expectException(\LengthException::class);
+
+        $observeeDataRetriever->getBirthdayFromMessage($message);
+    }
+
+    /**
+     * @test
+     */
+    public function retrievesBirthdayFromMessageCorrectly()
+    {
+        $userRetrieverStub = $this->createMock(UserRetriever::class);
+        $observeeDataRetriever = new ObserveeDataRetriever($userRetrieverStub);
+        $message = 'add 1 13.10.1996';
+        $actual = $observeeDataRetriever->getBirthdayFromMessage($message);
+        $expected = '1996-10-13';
+
+        $this->assertEquals($expected, $actual);
     }
 }
