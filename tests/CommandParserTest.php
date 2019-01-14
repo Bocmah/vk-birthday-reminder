5e748128d7d812cd46827cf83ca694dc6673c013
Artem <atrushkin1@gmail.com>
2019-01-14 22:49:01 +0300

    (test) CommandParser

---
+++ tests/CommandParserTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bocmah
+ * Date: 14/01/2019
+ * Time: 22:41
+ */
+
+namespace VkBirthdayReminder\Tests;
+
+use PHPUnit\Framework\TestCase;
+use VkBirthdayReminder\Helpers\CommandParser;
+use VkBirthdayReminder\Commands;
+
+class CommandParserTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider commandsProvider
+     */
+    public function retrievesCommandsCorrectly($inputCommand, $expectedOutputCommand)
+    {
+        $parser = new CommandParser();
+        $this->assertEquals($expectedOutputCommand, $parser->parse($inputCommand));
+    }
+
+    public function commandsProvider()
+    {
+        return [
+            ['Add 1 13.10.1996', Commands::ADD],
+            ['list', Commands::LIST],
+            ['update 823452 12.12.1990', Commands::UPDATE],
+            ['delete 2523', Commands::DELETE],
+            ['nOtIfy', Commands::NOTIFY],
+            ['help', Commands::HELP],
+            ['gklasdrei', Commands::UNKNOWN]
+        ];
+    }
+}
