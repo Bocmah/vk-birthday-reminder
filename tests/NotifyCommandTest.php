f24500312d188e88bf90bd9830cbd1aa4b3cd1e9
Artem <atrushkin1@gmail.com>
2019-01-17 22:11:25 +0300

    (test) create NotifyCommandTest

---
+++ tests/NotifyCommandTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace VkBirthdayReminder\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class NotifyCommandTest extends TestCase
+{
+    protected $notifyCommand;
+
+    /**
+     * @test
+     */
+    public function setsNotifyTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
