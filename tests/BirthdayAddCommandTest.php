80f4521d39255d04e59eb2c3d374d68a5e9bca4e
Artem <atrushkin1@gmail.com>
2019-01-23 23:53:07 +0300

    (test) create BirthdayAddCommandTest

---
+++ tests/BirthdayAddCommandTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace VkBirthdayReminder\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class BirthdayAddCommandTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function correctlyRespondsToInvalidDateOfBirth()
+    {
+
+    }
+}
