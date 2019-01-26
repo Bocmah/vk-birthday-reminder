0af5fae815c666b269639868257286d731537222
Artem <atrushkin1@gmail.com>
2019-01-26 15:07:32 +0300

    (test) UpdateCommandTest::correctlyRespondsToMessageValidationViolations()

--- tests/UpdateCommandTest.php
+++ tests/UpdateCommandTest.php
@@ -13,8 +13,10 @@ use Doctrine\ORM\EntityRepository;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use VkBirthdayReminder\Commands\UpdateCommand;
+use VkBirthdayReminder\Entities\Observer;
 use VkBirthdayReminder\Helpers\MessageSender;
 use VkBirthdayReminder\Helpers\ObserveeDataRetriever;
+use VkBirthdayReminder\Helpers\UserRetriever;
 
 class UpdateCommandTest extends TestCase
 {
@@ -53,5 +55,62 @@ class UpdateCommandTest extends TestCase
         );
 
         $updateCommand->execute();
+    }
+
+    public function validationViolationsProvider()
+    {
+        return [
+            ['Update 532464241 32.09.2000', '2000-09-32', ['info'], "Обнаружены ошибки:\n\nДата неправильная. Она должна быть в формате DD.MM.YYYY. Например: 13.10.1996.\n"],
+            ['Update fjwekwlelrfrkgkrelreti 30.09.2000', '2000-09-30', ['error' => true], "Обнаружены ошибки:\n\nЮзер с таким id не найден в VK.\n"],
+            ['Update fjwekwlelrfrkgkrelreti 32.09.2000', '2000-09-32', ['error' => true], "Обнаружены ошибки:\n\nЮзер с таким id не найден в VK.\nДата неправильная. Она должна быть в формате DD.MM.YYYY. Например: 13.10.1996.\n"]
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider validationViolationsProvider
+     * @param $receivedMessage
+     * @param $birthday
+     * @param $vkUserObject
+     * @param $messageToSend
+     */
+    public function correctlyRespondsToMessageValidationViolations(
+        $receivedMessage,
+        $birthday,
+        $vkUserObject,
+        $messageToSend
+    ) {
+        $msgStub = new \stdClass();
+        $msgStub->from_id = 5;
+        $msgStub->text = $receivedMessage;
+
+        /** @var MessageSender|MockObject $messageSenderStub */
+        $messageSenderStub = $this->createMock(MessageSender::class);
+        $messageSenderStub->expects($this->once())
+            ->method('send')
+            ->with($messageToSend, $msgStub->from_id);
+
+        $observerStub = $this->createMock(Observer::class);
+
+        $repositoryStub = $this->createMock(EntityRepository::class);
+        $repositoryStub->method('findOneBy')->willReturn($observerStub);
+
+        /** @var EntityManager|MockObject $entityManagerStub */
+        $entityManagerStub = $this->createMock(EntityManager::class);
+        $entityManagerStub->method('getRepository')->willReturn($repositoryStub);
+
+        /** @var ObserveeDataRetriever|MockObject $observeeRetrieverStub */
+        $observeeRetrieverStub = $this->createMock(ObserveeDataRetriever::class);
+        $observeeRetrieverStub->method('getBirthdayFromMessage')->willReturn($birthday);
+        $observeeRetrieverStub->method('getVkUserObjectFromMessage')->willReturn($vkUserObject);
+
+        $birthdayAddCommand = new UpdateCommand(
+            $msgStub,
+            $messageSenderStub,
+            $entityManagerStub,
+            $observeeRetrieverStub
+        );
+
+        $birthdayAddCommand->execute();
     }
 }
